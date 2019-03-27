fab5de48aee4ec1155497ea01a4cb2a01cc8e408
XinLiu1223 <xinliu1223@gmail.com>
2019-03-27 16:59:23 -0400

    add more code

--- welcome.js
+++ welcome.js
@@ -186,3 +186,34 @@ var squareRoot = numbers2.myMap(function(num) {
 
 console.log(goodT);
 console.log(squareRoot);
+
+
+/*****************NATIVE REDUCE*************************/
+
+
+Array.prototype.myReduce = function(callback, initialVal) {
+  var accumulator = initialVal ? initialVal : undefined;
+  for (var i = 0; i < this.length; i++) {
+      if (accumulator !== undefined)
+          accumulator = callback.call(undefined, accumulator, this[i], i, this);
+      else
+          accumulator = this[i]; // it is to assign the First Item value of array to accum
+  }
+  return accumulator;
+};
+
+//tests
+var numbers3 = [20, 20, 2, 3];
+var total = numbers3.myReduce(function(a, b) {
+  return a + b;
+}, 10);
+console.log(total); // 55
+
+var flattened = [
+  [0, 1],
+  [2, 3],
+  [4, 5]
+].myReduce(function(a, b) {
+  return a.concat(b);
+});
+console.log(flattened); //[ 0, 1, 2, 3, 4, 5 ]
