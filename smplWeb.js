c8b196f51ff516f07ecaa7598304e02af8acc1ad
XinLiu1223 <xinliu1223@gmail.com>
2019-04-02 14:40:43 -0400

    second commit on new code

--- smplWeb.js
+++ smplWeb.js
@@ -2,9 +2,14 @@ const http = require('http');
 
 const onRequest = (request, response) => {
   console.log('server received request');
-  response.writeHead(200, { 'Content-Type': 'text/plant' });
-  response.write('Hello from node web');
-  response.end();
+//   response.writeHead(200, { 'Content-Type': 'text/plant' });
+//   response.write('Hello from node web');
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  const myObj = {
+      name: 'Xin',
+      job: 'Developer'
+  };
+  response.end(JSON.stringify(myObj));
 };
 
 const server = http.createServer(onRequest);
